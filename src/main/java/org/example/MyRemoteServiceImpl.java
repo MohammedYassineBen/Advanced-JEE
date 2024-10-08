479094b19991c1443308e42cc2855592408230a1
Mohammed Yassine BENAOUIJA <mohammed2021brainy@gmail.com>
2024-10-08 12:30:23 +0100

    °°Latest commit for server side

--- src/main/java/org/example/MyRemoteServiceImpl.java
+++ src/main/java/org/example/MyRemoteServiceImpl.java
@@ -1,37 +1,12 @@
 package org.example;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MyRemoteServiceImpl extends UnicastRemoteObject implements MyRemoteService {
-    public MyRemoteServiceImpl() throws RemoteException {
-        super();
-    }
-
-    @Override
-    public String hello() throws RemoteException {
-        return "Hello From the RMI Server";
-    }
-
-    @Override
-    public String menu() throws RemoteException {
-        List<String> list = new ArrayList<String>();
-        list.add("Ajouter un livre");
-        list.add("Rechercher un livre");
-        list.add("Lister tous les livres");
-        list.add("Quitter");
-
-        String listString = "";
-        int index = 1;
-        for (String s : list)
-        {
-            listString += "\n"+index +"-" + s ;
-            index++;
-        }
-
-        return  listString ;
-    }
-
+public interface MyRemoteService extends Remote {
+    String hello() throws RemoteException;
+    String menu() throws RemoteException;
+    String ajouterLivre(String titre) throws RemoteException;
+    String rechercheLivre(String titre) throws RemoteException;
+    String listerTousLesLivres() throws RemoteException;
 }
--- src/main/java/org/example/RMIServer.java
+++ src/main/java/org/example/RMIServer.java
@@ -1,20 +1,18 @@
 package org.example;
 
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.RemoteException;
 
 public class RMIServer {
-    public static void main(String[] args) throws RemoteException {
-
-
-                try {
-                    MyRemoteService service = new MyRemoteServiceImpl();
-                    Registry registry = LocateRegistry.createRegistry(1099);
-                    registry.rebind("MyRemoteService", service);
-                    System.out.println("RMI Server ready");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public static void main(String[] args) {
+        try {
+            MyRemoteService service = new MyRemoteServiceImpl();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("MyRemoteService", service);
+            System.out.println("RMI Server ready");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
